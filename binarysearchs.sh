2e535870bae82963ea1a581c5a14d534af6dbab6
fathima <fathimanafeesavy@gmail.com>
2020-03-16 13:24:27 +0530

    [fathima]BinarySearchUpdated

--- binarysearchs.sh
+++ binarysearchs.sh
@@ -2,13 +2,13 @@
 echo "Enter the word to search"
 read pattern
 Y=O
-awk -v p="$pattern" 'BEGIN {IGNORECASE=1} $0 ~ p {Y++} END { print "CHECKING..."}' AWKfile.txt
-if [ Y==1 ]
-then
-echo "String Detected"
-else
-echo "cannot detect"
-fi
+awk -v p="$pattern" 'BEGIN {IGNORECASE=1} $0 ~ p{Y++} END { if(Y==1) print "String detected"; else print "Not detected"}' AWKfile.txt
+#if [ Y==1 ]
+#then
+#echo "String Detected"
+#else
+#echo "cannot detect"
+#fi
 
 
 
